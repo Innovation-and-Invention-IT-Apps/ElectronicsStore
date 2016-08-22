16c9afb74b8a76e6618d8438cc53424e7085df13
Kayaniranjankumar Reddy <kayaniranjankumar.r@HCL.COM>
2016-08-22 11:28:51 +0530

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from "@angular/core";
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
+
+@Component({
+  selector: "main",
+  directives: [NS_ROUTER_DIRECTIVES],
+  template: "<page-router-outlet></page-router-outlet>"
+})
+export class AppComponent {}
+/*
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: "<Label text='hello NativeScript'></Label>"
+})
+export class AppComponent {}
+*/
